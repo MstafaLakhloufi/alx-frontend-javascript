6df2bdd34df114666c8360a7079e8b4c4236b806
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2024-10-02 14:20:09 +0000

    update task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
+/*eslint-disable*/
 export default function taskBlock(trueOrFalse) {
-  const task = false;  // Using const as task won't change
-  const task2 = true;  // Using const as task2 won't change
-
-  if (trueOrFalse) {
-    const task = true;  // This is scoped to the if block
-    const task2 = false; // This is scoped to the if block
-  }
-
-  return [task, task2];  // The original task and task2 values are returned
+	const task = false;
+	const task2 = true;
+	if (trueOrFalse) {
+		const task = true;
+		const task2 = false;
+	}
+	return [task, task2];
 }
