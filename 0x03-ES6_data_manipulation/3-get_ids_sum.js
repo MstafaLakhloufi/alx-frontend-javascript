9b407a20c0c6ec8532a110444c18c2abe5f86361
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2024-10-08 22:44:45 +0000

    data manipulation task 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
-export default function getStudentsIdsSum(students){
-    return students.reduce((sum, { id }) => sum + id, 0);
+export default function getStudentIdsSum(students) {
+  return students.reduce((counter, currentObj) => counter + currentObj.id, 0);
 }
