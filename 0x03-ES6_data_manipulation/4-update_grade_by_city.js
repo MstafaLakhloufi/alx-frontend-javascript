6983a2c5c42e1f2fb9f743e971580626f635ab02
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2024-10-08 22:48:07 +0000

    data manipulation task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,3 @@
-export default function updateStudentGradeByCity (students, city, newGrades) {
-	return students
-		.filter(student => student.location === city)
-		.map(student => {
-			const grades = newGrades.find(grade => grade.studentId === student.id);
-			return {
-				...student,
-				grade: grades ? grades.grade : 'N/A'
-			};
-		});
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students.filter((student) => student.location === city).map((student) => ({ ...student, grade: newGrades.filter((g) => g.studentId === student.id).map((g) => g.grade)[0] || 'N/A' }));
 }
