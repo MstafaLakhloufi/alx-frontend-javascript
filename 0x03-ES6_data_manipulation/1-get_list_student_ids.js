f21acaa338560d549e63aa29da991cc13696ea2f
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2024-10-08 22:33:31 +0000

    data manipulation task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 export default function getListStudentIds(students) {
-	if (Array.isArray(students)) {
-		return students.map((obj) => obj.id);
-	}
-	return [];
+  if (Array.isArray(students)) {
+    return students.map((item) => item.id);
+  }
+  return [];
 }
